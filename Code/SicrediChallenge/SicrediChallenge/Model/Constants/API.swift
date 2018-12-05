3e9b995cae48880a1bd1695e60c3e24b6989e667
Bruno Dorneles <>
2018-12-05 12:29:17 -0200

    added API  struct for API constants and Request class

---
+++ Code/SicrediChallenge/SicrediChallenge/Model/Constants/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  SicrediChallenge
+//
+//  Created by Bruno Dorneles on 05/12/18.
+//  Copyright © 2018 Bruno. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    static let event_url = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events"
+    static let event_detail = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events/%@"
+    static let checkin_url = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/checkin"
+}
