db82670f3dc73ee12f3ba645d1473899328d99e5
Bruno Dorneles <>
2018-12-05 23:19:13 -0200

    created mainviewcontroller and mainviewmodel, assigned new mainviewcontroller into storyboard

---
+++ Code/SicrediChallenge/SicrediChallenge/ViewControllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  SicrediChallenge
+//
+//  Created by Bruno Dorneles on 05/12/18.
+//  Copyright © 2018 Bruno. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+var mainViewModel = MainViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
